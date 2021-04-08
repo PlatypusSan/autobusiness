309505e6e6b80ced2cdfcb99a96c2f36b5934f35
PlatypusSan <ilyabgtrv@gmail.com>
2021-04-08 10:34:00 +0300

    API version added

--- src/main/java/com/test/autobusiness/controllers/CarController.java
+++ src/main/java/com/test/autobusiness/controllers/CarController.java
@@ -13,6 +13,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("api/v1")
 public class CarController {
 
     private final CarService carService;
--- src/main/java/com/test/autobusiness/controllers/DetailsController.java
+++ src/main/java/com/test/autobusiness/controllers/DetailsController.java
@@ -3,11 +3,13 @@ package com.test.autobusiness.controllers;
 import com.test.autobusiness.entities.dto.detailsdto.DetailsResponse;
 import com.test.autobusiness.services.DetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
 
 @RestController
+@RequestMapping("api/v1")
 public class DetailsController {
 
     private final DetailsService detailsService;
