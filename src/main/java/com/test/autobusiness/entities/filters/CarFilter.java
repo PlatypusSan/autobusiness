3b9bcc76c1ae011649f564f84bfcd5ed08c1de02
PlatypusSan <ilyabgtrv@gmail.com>
2021-05-14 15:54:44 +0300

    CarFilter fix

--- src/main/java/com/test/autobusiness/entities/filters/CarFilter.java
+++ src/main/java/com/test/autobusiness/entities/filters/CarFilter.java
@@ -5,35 +5,18 @@ import lombok.Data;
 @Data
 public class CarFilter {
 
-    private String brand;
-    private String model;
-    private String generation;
-    private String body;
-    private String driveUnit;
-    private String transmission;
-    private String engineType;
-    private double minEngineVolume;
-    private double maxEngineVolume;
-    private int minAge;
-    private int maxAge;
-    private int maxMileAge;
-    private int minPrice;
-    private int maxPrice;
-
-    public CarFilter() {
-        this.brand = "";
-        this.model = "";
-        this.generation = "";
-        this.body = "";
-        this.driveUnit = "";
-        this.transmission = "";
-        this.engineType = "";
-        this.minEngineVolume = 0D;
-        this.maxEngineVolume = 9D;
-        this.maxMileAge = 1000000;
-        this.minAge = 1900;
-        this.maxAge = 2021;
-        this.minPrice = 0;
-        this.maxPrice = 10000000;
-    }
+    private String brand = "";
+    private String model = "";
+    private String generation = "";
+    private String body = "";
+    private String driveUnit = "";
+    private String transmission = "";
+    private String engineType = "";
+    private double minEngineVolume = 0D;
+    private double maxEngineVolume = 9D;
+    private int minAge = 1900;
+    private int maxAge = 2021;
+    private int maxMileAge = 1000000;
+    private int minPrice = 0;
+    private int maxPrice = 10000000;
 }
